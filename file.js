c790a991d05f443f5f1a99de8743c3178db7e233
Nick <scifi6546@protonmail.com>
2019-02-12 14:37:12 -0900

    test

--- file.js
+++ file.js
@@ -4,7 +4,7 @@ file.BaseDir = "./files/";
 file.load = function(file){
 	return fs.readFileSync(file,'utf8');
 }
-
+// I changes stuff!
 file.readDir = function(fileDir){
 	var temp = fs.readdirSync(this.BaseDir+fileDir);
 	console.log(temp);
