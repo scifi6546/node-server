d12d941a3982a298b6193dd0567fac53619a34b0
Nick <scifi6546@protonmail.com>
2019-02-12 17:25:36 -0900

    changed client javascript a little

--- html/index.js
+++ html/index.js
@@ -1,5 +1,8 @@
 console.log("loaded!");
-var html=`<div class = file>Hello World</div>`;
+var html=`
+<div class = file>
+    {{file_name}}
+</div>`;
 
 var files;
 $(document).ready(function(){start();});
@@ -9,9 +12,14 @@ function start(){
         files=jd.files;
 
         console.log(jd);
-
+        console.log(files);
         for(var i =0;i<files.length;i++){
-            $(".body").append(html);
+            console.log("appended")
+            var temp = html;
+            temp = temp.replace("{{file_name}}",files[i]);
+            var test = $(".body").append(temp);
+            console.log(test);
+            //$(test).text()=files[i];
         }
     });
 }
